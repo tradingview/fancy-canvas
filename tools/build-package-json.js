c243452a9eb3df4a19dcbcacbcc92eaa70fa0d81
Mark Silverwood <3482679+SlicedSilver@users.noreply.github.com>
2024-04-28 21:27:55 +0100

    drop commonjs support and bump target to es2020

--- tools/build-package-json.js
+++ tools/build-package-json.js
@@ -39,6 +39,6 @@ const output = {
 	type: input.type,
 
 	// contents
-	files: ['**/*.mjs', '**/*.d.mts', '**/*.js', '**/*.d.ts'],
+	files: ['**/*.js', '**/*.d.ts'],
 };
 fs.writeFileSync(outputPath, JSON.stringify(output, null, 2));
